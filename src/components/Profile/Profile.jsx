116ad44e66a04433c653f93ca94c8f36e6b04a98
Anatoliy Shulika <shulika.anatoliy2@gmail.com>
2022-12-10 20:33:15 +0200

    2

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -34,13 +34,15 @@ export default function Profile({ user }) {
 }
 
 Profile.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  stats: PropTypes.exact({
-    followers: PropTypes.number.isRequired,
-    views: PropTypes.number.isRequired,
-    likes: PropTypes.number.isRequired,
+  user: PropTypes.exact({
+    avatar: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    stats: PropTypes.exact({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }),
   }),
 };
