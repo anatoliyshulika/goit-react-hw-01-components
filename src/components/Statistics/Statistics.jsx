4254e063052ada593e6430699837370c12d0aaec
Anatoliy Shulika <shulika.anatoliy2@gmail.com>
2022-12-11 22:19:37 +0200

    Done

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,25 +1,38 @@
-export default function Statistics () {
-    return (<section class="statistics">
-    <h2 class="title">Upload stats</h2>
-  
-    <ul class="stat-list">
-      <li class="item">
-        <span class="label">.docx</span>
-        <span class="percentage">4%</span>
-      </li>
-      <li class="item">
-        <span class="label">.mp3</span>
-        <span class="percentage">14%</span>
-      </li>
-      <li class="item">
-        <span class="label">.pdf</span>
-        <span class="percentage">41%</span>
-      </li>
-      <li class="item">
-        <span class="label">.mp4</span>
-        <span class="percentage">12%</span>
-      </li>
-    </ul>
-  </section>
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+export default function Statistics({ title, stats }) {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.stat_list}>
+        {stats.map(data => (
+          <li
+            className={css.item}
+            key={data.id}
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
+            <span className={css.label}>{data.label}</span>
+            <span className={css.percentage}>{data.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
+};
